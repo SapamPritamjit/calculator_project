8a0b846573c22d27f982b008c592878d4de18e94
SapamPritamjit <pritamjitsapam@gmail.com>
2025-11-10 23:01:07 +0530

    Add modulus

--- calculator.c
+++ calculator.c
@@ -4,7 +4,7 @@ int main() {
     char operator;
     double num1, num2, result;
 
-    printf("Enter an operator (+, -, *, /): ");
+    printf("Enter an operator (+, -, *, /, %%): ");
     scanf(" %c", &operator);
     printf("Enter two numbers: ");
     scanf("%lf %lf", &num1, &num2);
@@ -22,6 +22,14 @@ int main() {
         case '/':
             if (num2 != 0)
                 result = num1 / num2;
+            else {
+                printf("Error: Division by zero!\n");
+                return 1;
+            }
+            break;
+        case '%':
+            if ((int)num2 != 0)
+                result = (int)num1 % (int)num2;
             else {
                 printf("Error: Division by zero!\n");
                 return 1;
